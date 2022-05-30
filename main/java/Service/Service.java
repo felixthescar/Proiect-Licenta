933465f9c8eb50bfaf5b855e58a0eba01ebc0d68
felixthescar <felixscarmoc2@protonmail.com>
2022-05-31 00:20:17 +0200

    Add files via upload

--- main/java/Service/Service.java
+++ main/java/Service/Service.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Repo.DatabaseRepo;
+import Domain.Sales;
 import Domain.Stock;
 import Domain.Users;
 
@@ -12,6 +13,20 @@ public class Service {
 	
 	public Service(DatabaseRepo repo) {
 		this.repo = repo;
+	}
+
+	public void tryOrder(int idPrd, String username, String adr, String dataC, String dataL, int status, int cant, int pret) {
+		//validation
+
+		//logic
+		repo.addOrder(idPrd, username, adr, dataC, dataL, status, cant, pret);
+	}
+
+	public int getPrice(String id) {
+		//validation
+
+		//logic
+		return repo.getPriceOfItem(id);
 	}
 	
 	public int tryLogin(String username, String password) throws Exception {
@@ -72,6 +87,15 @@ public class Service {
 		//logic
 		repo.modifyStonkWithID(id, man, mod, qntInt, priceInt);
 	}
+
+	public void tryModifyUsers(int id, String usr, String pass, String adm_prv) {
+		//validation
+		int adm_prvInt;
+		if(adm_prv.equals("")) adm_prvInt = 0;
+		else adm_prvInt = Integer.parseInt(adm_prv);
+		//logic
+		repo.modifyUserWithID(id, usr, pass, adm_prvInt);
+	}
     
     public void addStonksData(String manufacturer, String model, int quantity, int price) {
     	repo.addData(manufacturer, model, quantity, price);
@@ -80,6 +104,14 @@ public class Service {
     public void removeDataWithID(String query) {
     	repo.removeStonkWithID(query);
     }
+
+	public void removeUserDataWithID(String query) {
+    	repo.removeUserWithID(query);
+    }
+
+	public void deleteSalesData(int id) {
+		repo.removeSalesData(id);
+	}
 	
 	public List<String[]> stonkGetAllData() {
 		List<String[]> l = new ArrayList<>();
@@ -113,6 +145,41 @@ public class Service {
 		return l;
 	}
 
+	public List<String[]> userSearchData(String query) {
+		List<String[]> l = new ArrayList<>();
+		
+		for(Users user: repo.searchUsersProgressive(query)) {
+			l.add(new String[] {
+				Integer.toString(user.getID()),
+				user.getUsername(),
+				user.getPassword(),
+				Integer.toString(user.getAdminPriviliges())
+			});
+		}
+		
+		return l;
+	}
+
+	public List<String[]> salesSearchData(String query) {
+		List<String[]> l = new ArrayList<>();
+		
+		for(Sales sale: repo.searchSalesProgressive(query)) {
+			l.add(new String[] {
+				Integer.toString(sale.getId()),
+				Integer.toString(sale.getIdProd()),
+				sale.getUser(),
+				sale.getAdresa(),
+				sale.getDataC(),
+				sale.getDataL(),
+				Integer.toString(sale.getStatus()),
+				Integer.toString(sale.getCantitate()),
+				Integer.toString(sale.getPret())
+			});
+		}
+		
+		return l;
+	}
+
 	public List<String[]> userGetAllData() {
 		List<String[]> l = new ArrayList<>();
 		
@@ -127,13 +194,33 @@ public class Service {
 		
 		return l;
 	}
-	
+
+	public List<String[]> salesSearchAllData() {
+		List<String[]> l = new ArrayList<>();
+		
+		for(Sales sale: repo.getAllSales()) {
+			l.add(new String[] {
+				Integer.toString(sale.getId()),
+				Integer.toString(sale.getIdProd()),
+				sale.getUser(),
+				sale.getAdresa(),
+				sale.getDataC(),
+				sale.getDataL(),
+				Integer.toString(sale.getStatus()),
+				Integer.toString(sale.getCantitate()),
+				Integer.toString(sale.getPret())
+			});
+		}
+		
+		return l;
+	}
 	
 	public List<String[]> stonkSearchUserData(String query) {
 		List<String[]> l = new ArrayList<>();
 		
 		for(Stock stock: repo.searchStocksProgressive(query)) {
 			l.add(new String[] {
+					Integer.toString(stock.getID()),
 					stock.getManufacturer(),
 					stock.getModel(),
 					Integer.toString(stock.getPrice())
